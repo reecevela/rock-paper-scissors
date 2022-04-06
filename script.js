eb435ace3a7923698d3489aa0f9203cb820dad87
Reece Vela <reecevela@outlook.com>
2022-04-06 18:45:18 -0500

    Add JS Functionality

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+const cDefaultMessage = 'The Computer is waiting for you';
+const cChoseMessage = 'The Computer chose';
+
+let pChoice = "";
+
 function pChoose(choice) {
-  document.getElementById("paper").style.color = "red";
+    pChoice = choice;
+    document.getElementById("p-message").textContent = `You chose ${pChoice}`;
+    cChoose();
 }
+
+function cChoose() {
+  const message = document.getElementById('c-message');
+  let cChoice = '';
+
+  switch (pChoice) {
+    case 'rock':
+      cChoice = ' paper';
+      break;
+    case 'paper':
+      cChoice = ' scissors';
+      break;
+    case 'scissors':
+      cChoice = ' rock';
+  }
+
+  message.textContent = cChoseMessage + cChoice;
+}
+
