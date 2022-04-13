85b8f7dc5dad0c0ca6e38c3cfd3b3bffad6a6cd2
Reece Vela <reecevela@outlook.com>
2022-04-12 20:22:19 -0500

    Change display method to alerts

--- script.js
+++ script.js
@@ -1,3 +1,77 @@
+const cDefaultMessage = 'The Computer is waiting for you';
+const cChoseMessage = 'The Computer chose';
+
+let pChoice = "";
+let cChoice = "";
+let pMessage = "";
+
 function pChoose(choice) {
-  document.getElementById("paper").style.color = "red";
+    pChoice = choice;
+    const pMessage = document.getElementById("p-message");
+
+    pMessage.textContent = `You chose ${pChoice}`;
+    alert(pMessage.textContent +  `\n (Press Enter or Ok)`);
+    cChoose();
+    displayWinner();
+}
+
+function cChoose() {
+  const message = document.getElementById('c-message');
+
+  cChoice = pickRandomOfThree();
+  message.textContent = cChoseMessage + " " + cChoice;
+  alert(message.textContent + ` \n (Press Enter or Ok)`);
+
+  function pickRandomOfThree(one = "rock", two = "paper", three = "scissors") {
+    const randNum = Math.floor(Math.random()*3);
+    switch (randNum) {
+      case 0:
+        return one;
+      case 1:
+        return two;
+      case 2: 
+        return three;
+      default:
+        return "There's a bug"
+    }
+  }
+}
+
+function displayWinner() {
+  const winner = evaluateGame();
+
+  alert(`Winner: ${winner} \n (Enter or Ok to play again)`);
+
+  function evaluateGame(){
+    if (pChoice === cChoice) {
+      return "tie";
+    }
+    switch (pChoice) {
+      case "rock":
+        switch (cChoice) {
+          case "paper":
+            return "computer";
+          case "scissors":
+            return "player"
+        }
+      case "paper":
+        switch (cChoice) {
+          case "rock":
+            return "player";
+          case "scissors":
+            return "computer";
+        }
+      case "scissors":
+        switch (cChoice) {
+          case "paper":
+            return "player";
+          case "rock":
+            return "computer";
+        }
+    }
+  }
+}
+
+function resetRound(){
+
 }
